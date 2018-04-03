13caf8194b65a13dd959760b69e74f56e4ed4e76
George Omnet <georgeomnet@gmail.com>
2018-04-03 21:00:08 +0100

    Update Bot Interface.py

--- Bot Interface.py
+++ Bot Interface.py
@@ -15,4 +15,4 @@ async def on_ready():
         if out != "":
             await bot.send_message(discord.Object(id="413755435338956811"),out)
 
-bot.run('NDI5NzUwNTI5NzM2ODM1MDc0.DaQ1pg.AZkv3BTcOAWHCc_QN0E83bZdXOs')
+bot.run('key')
