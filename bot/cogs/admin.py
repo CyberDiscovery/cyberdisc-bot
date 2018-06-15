32e97306fdf1f2093bc6e0e04da0c82e661bb241
Sam Wedgwood <sjwedgwood@gmail.com>
2018-06-15 16:02:27 +0100

    Some more changes, and a little bit of flake8

--- bot/cogs/admin.py
+++ bot/cogs/admin.py
@@ -1,7 +1,7 @@
+from bot.constants import ADMIN_ROLES, BANNED_DOMAINS
+
 from discord import Member
 from discord.ext.commands import Bot, Context, command, has_any_role
-
-from bot.constants import ADMIN_ROLES, BANNED_DOMAINS
 
 
 class Admin:
@@ -14,7 +14,7 @@ class Admin:
 
     async def mute_member(self, member: Member, reason: str="N/A"):
         self.bot.muted.append(member.id)
-        print(f"Member {member} ({member.id}) has been muted for reason: {reason}")
+        print(f"Member {member} ({member.id}) has been muted: {reason}")
 
     @command()
     @has_any_role(*ADMIN_ROLES)
@@ -32,10 +32,12 @@ class Admin:
         await ctx.send(f"{ctx.author.mention} | {member.mention} has been unmuted.")
 
     async def on_message(self, message):
+        # Check if author is muted
         if message.author.id in self.bot.muted:
             await message.delete()
             await message.author.send("You are muted!")
         
+        # Check if message contains a banned domain
         elif any(domain in message.content.lower() for domain in BANNED_DOMAINS):
             await message.delete()
             await message.channel.send(f"{message.author.mention} | That domain is banned! You have been muted.")
