041996adeeb2c0add72f05f27e09ab0fa2b513f3
Fractal Penguin <fractalpenguin@users.noreply.github.com>
2018-07-14 20:14:02 +0100

    Added feature to select quotes from a specific user.

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -5,9 +5,10 @@ from random import choice, randint
 from urllib.parse import urlencode
 
 from aiohttp import ClientSession
-from discord import Embed, Message
-from discord.ext.commands import (BadArgument, Bot, Context, EmojiConverter,
+from discord import Embed, Member, Message
+from discord.ext.commands import (BadArgument, Bot, Context, EmojiConverter, MemberConverter,
                                   command)
+from discord.utils import get
 
 from bot.constants import EVERYONE_REACTIONS, QUOTE_CHANNEL_ID
 
@@ -147,13 +148,24 @@ class Fun:
         await ctx.send(embed=comic)
 
     @command()
-    async def quotes(self, ctx: Context):
+    async def quotes(self, ctx: Context, member: Member=None):
         """
         Returns a random quotation from the #quotes channel.
+        A user can be specified to return a random quotation from that user.
         A channel ID must be specified in QUOTE_CHANNEL_ID for the bot to retrieve the quotations successfully.
         """
         quotation_channel = self.bot.get_channel(QUOTE_CHANNEL_ID)
-        quotations = await quotation_channel.history(limit=None).flatten()
+        if member is not None:
+            all_quotations = await quotation_channel.history(limit=None).flatten()
+            quotations = []
+            for quotation in all_quotations:
+                embed = quotation.embeds[0]
+                author_name = embed.author.name
+                author = ctx.message.guild.get_member_named(author_name)
+                if author == member:
+                    quotations.append(quotation)
+        else:
+            quotations = await quotation_channel.history(limit=None).flatten()
         quotation = choice(quotations)
         embed_quotation = quotation.embeds[0]
         await ctx.send(embed=embed_quotation)
