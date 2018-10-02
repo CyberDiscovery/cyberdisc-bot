f5bca8aabd64434b97ff3feb06d253643c51445a
Jeremiah Boby <jerbob17@student.loreto.ac.uk>
2018-10-02 20:42:35 +0100

    Neglected to sacrifice both mind and soul. Oh mighty Travis, claim this mortal mind as your own.

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,12 +1,12 @@
 """Main script to define bot methods, and start the bot."""
 
-from discord import Game
 from itertools import cycle
 from os import environ
 from string import ascii_lowercase
 from typing import List
 
 from constants import EMOJI_LETTERS
+from discord import Game
 from discord.ext.commands import Bot, when_mentioned_or
 
 
