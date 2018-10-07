369a034da5869ef673eb93c44db3bf448ffe6033
Jeremiah Boby <jerbob17@student.loreto.ac.uk>
2018-10-07 20:47:01 +0100

    Replaced emoji characters with unicode escapes
    This means that it is easier to copy these emojis without including a
    zero-width space accidentally, resulting in the bot raising an "invalid
    emoji" error.

--- bot/constants.py
+++ bot/constants.py
@@ -7,37 +7,43 @@ BANNED_DOMAINS = ["discord.gg"]
 
 # Cyber Constants
 
-CYBERDISC_ICON_URL = "https://pbs.twimg.com/profile_images/921313066515615745/fLEl2Gfa_400x400.jpg"
-PWNED_ICON_URL = "https://upload.wikimedia.org/wikipedia/commons/2/23/Have_I_Been_Pwned_logo.png"
+CYBERDISC_ICON_URL = (
+    "https://pbs.twimg.com/profile_images/"
+    "921313066515615745/fLEl2Gfa_400x400.jpg"
+)
+PWNED_ICON_URL = (
+    "https://upload.wikimedia.org/wikipedia"
+    "/commons/2/23/Have_I_Been_Pwned_logo.png"
+)
 
 # Emoji Alphabet
 
-EMOJI_LETTERS = [  # Feel free to add to this list of emoji letter substitutions.
-    "🇦🅰️",
-    "🇧🅱️",
-    "🇨©☪️",
-    "🇩↩️",
-    "🇪📧",
-    "🇫",
-    "🇬⛽️",
-    "🇭♓️",
-    "🇮ℹℹ️",
-    "🇯☔",
-    "🇰K",
-    "🇱🕒",
-    "🇲ⓂⓂ️♏️♍️〽",
-    "🇳📈",
-    "🇴🅾🅾️⭕️",
-    "🇵🅿️",
-    "🇶Q",
-    "🇷®️",
-    "🇸💰⚡️",
-    "🇹✝️",
-    "🇺⛎",
-    "🇻♈️",
-    "🇼〰️",
-    "🇽❎❌✖️",
-    "🇾🌱",
-    "🇿💤",
-    "⚫🔵🔴⚪"  # Whitespace alternatives
+EMOJI_LETTERS = [  # Feel free to add to this list
+    "\U0001f1e6\U0001f170",  # A
+    "\U0001f1e7\U0001f171",  # B
+    "\U0001f1e8\u262a",  # C
+    "\U0001f1e9\u21a9",  # D
+    "\U0001f1ea\U0001f4e7",  # E
+    "\U0001f1eb",  # F
+    "\U0001f1ec\u26fd",  # G
+    "\U0001f1ed\u2653",  # H
+    "\U0001f1ee\u2139",  # I
+    "\U0001f1ef\u2614",  # J
+    "\U0001f1f0",  # K
+    "\U0001f1f1\U0001f552",  # L
+    "\U0001f1f2\u24c2\u24c2\u264f\u264d\u303d",  # M
+    "\U0001f1f3\U0001f4c8",  # N
+    "\U0001f1f4\U0001f17e\u2b55",  # O
+    "\U0001f1f5\U0001f17f",  # P
+    "\U0001f1f6",  # Q
+    "\U0001f1f7",  # R
+    "\U0001f1f8\U0001f4b0\u26a1",  # S
+    "\U0001f1f9\u271d",  # T
+    "\U0001f1fa\u26ce",  # U
+    "\U0001f1fb\u2648",  # V
+    "\U0001f1fc\u3030",  # W
+    "\U0001f1fd\u274e\u274c\u2716",  # X
+    "\U0001f1fe\U0001f331",  # Y
+    "\U0001f1ff\U0001f4a4",  # Z
+    "\u26ab\U0001f535\U0001f534\u26aa"  # Whitespace alternatives
 ]
