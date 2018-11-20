e53a15552d2da6919a9d29728689bb05c6578882
Alfie <42497992+AvrooVulcan@users.noreply.github.com>
2018-11-20 22:37:00 +0000

    Refactored assess challenge check  (#67)

--- bot/cogs/cyber.py
+++ bot/cogs/cyber.py
@@ -81,7 +81,7 @@ class Cyber:
         with open("assess.json") as f:
             assess_docs = load(f)
 
-        if challenge_num not in range(len(assess_docs) + 1):
+        if 0 < challenge_num <= len(assess_docs):
             await ctx.send("Invalid challenge number!")
 
         else:
