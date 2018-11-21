0a2534d2300d7d4aea09a10fc4ed4554864736ab
Sam Wedgwood <sjwedgwood@gmail.com>
2018-11-21 18:10:54 +0000

    Auto Deploy (#68)
    * modified and added files for deployment
    
    * whoops that thing.yml shouldn't have been there

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Build and deploy only on master
+if [ $TRAVIS_BRANCH == 'master' ] && [ $TRAVIS_PULL_REQUEST == 'true' ]; then
+    echo "Building image..."
+    docker build -t cyberdiscovery/cyberdisc-bot:latest .
+
+    echo "Connecting to docker hub..."
+    docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+
+    echo "Pushing image..."
+    docker push cyberdiscovery/cyberdisc-bot:latest
+
+    echo "Deploying..."
+    chmod 400 id_rsa
+    ssh deploy@$DEPLOY_HOST -n -o "StrictHostKeyChecking=no" -i id_rsa
+fi
