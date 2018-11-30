994c5c532c791d4cc0a39ddfd289af575e10459c
ddevlop <44953913+ddevlop@users.noreply.github.com>
2018-11-30 22:55:19 +0000

    fix imports sotp

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -17,7 +17,7 @@ from wand.drawing import Drawing
 from wand.image import Image
 
 from bot.constants import ADMIN_ROLES, EMOJI_LETTERS, FAKE_STAFF_ROLE_ID, QUOTES_BOT_ID, QUOTES_CHANNEL_ID,\
-    STAFF_ROLE_ID, SERVER_ID
+    SERVER_ID, STAFF_ROLE_ID
 
 
 EMOJI_LETTERS = [
