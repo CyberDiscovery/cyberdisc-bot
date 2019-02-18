b2a7385c011d0f35abf877a925c88062dd533bf2
Daniel Milnes <thebeanogamer@gmail.com>
2019-02-18 22:16:24 +0000

    Add code to initialise Sentry

--- cdbot/__init__.py
+++ cdbot/__init__.py
@@ -4,7 +4,11 @@ from os import environ
 
 from .bot import bot
 
+import sentry_sdk
+
 
 def main():
     """Entry point for poetry script."""
+    if environ.get("SENTRY_URL") is not None:
+        sentry_sdk.init(environ.get("SENTRY_URL"))
     bot.run(environ.get("BOT_TOKEN"))
