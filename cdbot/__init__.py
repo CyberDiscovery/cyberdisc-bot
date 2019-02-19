c6b91dbda517d4a57d4d97465843e116c7300d4a
Daniel Milnes <thebeanogamer@gmail.com>
2019-02-19 08:53:58 +0000

    Setup Sentry 2.0 (#149)
    * Add code to initialise Sentry
    
    * Calm down Travis
    
    * Fix lockfile
    
    * Travis makes me cry

--- cdbot/__init__.py
+++ cdbot/__init__.py
@@ -2,9 +2,13 @@
 
 from os import environ
 
+import sentry_sdk
+
 from .bot import bot
 
 
 def main():
     """Entry point for poetry script."""
+    if environ.get("SENTRY_URL") is not None:
+        sentry_sdk.init(environ.get("SENTRY_URL"))
     bot.run(environ.get("BOT_TOKEN"))
