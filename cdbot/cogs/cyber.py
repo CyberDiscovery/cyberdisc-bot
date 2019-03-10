19dc9026750be1691c0f56a21760a9c1b5d26153
Yudhajit N <28938427+Sh3llcod3@users.noreply.github.com>
2019-03-10 19:04:55 +0000

    Added the :readme command from scratch.
    * Allows ROOT_ROLE_ID users to download a JSON config for the readme. 
    * They can then tweak and use that readme file to create the readme on 
    any server.
    * This is essentially something that allows hotplugging READMEs on any 
    server. Solves #147. Cool huh?
    * Also, worth mentioning that this is my first bot feature (I've 
    literally never touched discord.py before this) and during the 3 days or 
    so of dev time that I gave it, I tried my best to write good code that 
    flake8 approves and to avoid merge conflicts. If this feature is 
    successful, then I'll write the soundboard later if revision and exams 
    do not obliterate me.

--- cdbot/cogs/cyber.py
+++ cdbot/cogs/cyber.py
@@ -2,16 +2,18 @@ import datetime
 import random
 import re
 import string
+from asyncio import sleep
 from hashlib import sha1
+from io import StringIO
 from json import load
 
 from aiohttp import ClientSession
 from cdbot.constants import (
-    BASE_ALIASES, CYBERDISC_ICON_URL, HINTS_LIMIT, PWNED_ICON_URL
+    BASE_ALIASES, CYBERDISC_ICON_URL, HINTS_LIMIT, PWNED_ICON_URL, ROOT_ROLE_ID
 )
 from dateutil.parser import parse
 from dateutil.relativedelta import relativedelta
-from discord import Embed, Message
+from discord import Colour, Embed, File, Message
 from discord.ext.commands import Bot, Cog, Context, command
 
 
@@ -85,7 +87,7 @@ class Cyber(Cog):
             return
 
         # Gather data from CyberStart Game.
-        with open("bot/data/game.json") as f:
+        with open("cdbot/data/game.json") as f:
             game_docs = load(f)
         # Temporary change to allow old usage method
         if base.isnumeric():
@@ -155,6 +157,150 @@ class Cyber(Cog):
 
         await ctx.send(embed=embed)
 
+    @command()
+    async def readme(self, ctx: Context, operand: str = "", channel_id: int = 0, msg_send_interval: int = 0):
+        """
+        Allows generating, sending and manipulation of JSON file containing the info needed
+        to create and send the embeds for the #readme channel. Only ROOT_ROLE_ID users have
+        the permissions need to use this command.
+        """
+
+        README_SEND_ALIASES = ["create", "push", "generate", "send", "make", "build", "upload"]
+        README_RECV_ALIASES = ["fetch", "get", "pull", "download", "retrieve", "dm", "dl"]
+
+        operand = operand.lower()
+
+        # The supplied operand is incorrect.
+        if not (operand in README_SEND_ALIASES + README_RECV_ALIASES):
+            incorrect_operand_embed = Embed(
+                colour=0x673ab7,
+                description=":shrug: **Invalid readme operand supplied.**"
+            )
+            await ctx.message.delete()
+            await ctx.send(embed=incorrect_operand_embed)
+
+        # User missed out the channel_id for certain commands.
+        elif (channel_id == 0 and operand in README_SEND_ALIASES):
+            misssing_channel_embed = Embed(
+                colour=0xff5722,
+                description=":facepalm: **Whoops, you missed out the channel ID! Try again.**"
+            )
+            await ctx.message.delete()
+            await ctx.send(embed=misssing_channel_embed)
+
+        # Process the request.
+        elif ROOT_ROLE_ID in [msg_author.id for msg_author in ctx.message.author.roles]:
+            # Let's create a series of #readme-capable embeds. If something is uploaded,
+            # It will attempt to use that file for the readme, if omitted, it will use
+            # the default JSONifed readme file and send that into the channel instead.
+            if operand in README_SEND_ALIASES:
+                try:
+                    usr_confirmation_embed = Embed(
+                        colour=0x4caf50,
+                        description=":white_check_mark: **Creating readme using uploaded config file.**"
+                    )
+
+                    # The user has uploaded a config.
+                    if ctx.message.attachments != []:
+                        json_file_location = [_.url for _ in ctx.message.attachments][0]
+
+                        # GETs the attachment data.
+                        async with ClientSession() as session:
+                            async with session.get(json_file_location) as response:
+                                if response.status == 200:
+                                    resp_text = await response.text()
+
+                        json_config = load(StringIO(resp_text))
+                        await ctx.send(embed=usr_confirmation_embed)
+
+                    # No config uploaded, just use default config file.
+                    else:
+                        with open("cdbot/data/readme.json", "rb") as default_json:
+                            json_config = load(default_json)
+
+                        usr_confirmation_embed.description = (":ballot_box_with_check: "
+                                                              "**Creating readme using default config file.**")
+                        await ctx.send(embed=usr_confirmation_embed)
+
+                    await ctx.message.delete()
+
+                    for section in json_config:
+                        # Get data from the JSON file sections.
+                        if "content" in json_config[section]:
+                            msg_content = json_config[section]["content"]
+
+                        # We have an embed. Call in the Seahawks.
+                        if "embed" in json_config[section]:
+                            current_embed = Embed()
+                            msg_embed = json_config[section]["embed"]
+                            if "text" in msg_embed:
+                                current_embed.description = msg_embed["text"]
+                            if "color" in msg_embed:
+                                current_embed.colour = Colour(int(msg_embed["color"], 16))
+
+                            # Parse the fields, if there are any.
+                            if "fields" in msg_embed:
+                                for current_field in msg_embed["fields"]:
+                                    # Add the fields to the current embed.
+                                    current_embed.add_field(
+                                        name=current_field["name"],
+                                        value=current_field["value"]
+                                    )
+
+                        # Send the message.
+                        requested_channel = self.bot.get_channel(channel_id)
+
+                        if ("msg_content" in locals() and "current_embed" not in locals()):
+                            await requested_channel.send(content=msg_content)
+                        elif ("current_embed" in locals() and "msg_content" not in locals()):
+                            await requested_channel.send(embed=current_embed)
+                        else:
+                            await requested_channel.send(content=msg_content, embed=current_embed)
+
+                        # User has requested a delay between each message being sent.
+                        if (0 < msg_send_interval < 901):
+                            await sleep(msg_send_interval)
+
+                except(Exception) as e:
+                    parse_fail_embed = Embed(
+                        colour=0x673ab7,
+                        description=":x: **Error parsing JSON file, please ensure its valid!**"
+                    )
+                    await ctx.message.delete()
+                    await ctx.send(embed=parse_fail_embed)
+                    await ctx.send(e)  # DEBUG
+
+            # Pull the readme JSON constant files and slide it into the user's DMs.
+            elif operand in README_RECV_ALIASES:
+                # Get the human-readble readme data.
+                with open("cdbot/data/readme_raw.json", "rb") as readme_json:
+                    raw_json = readme_json.read()
+
+                    # Slide it to the user's DMs.
+                    requesting_user = await self.bot.get_user_info(ctx.message.author.id)
+                    await requesting_user.send(
+                        content="Hey, here's your readme config file!",
+                        file=File(raw_json, 'readme_raw.json')
+                    )
+
+                    msg_confirmation = Embed(
+                        colour=0x009688,
+                        description=":airplane: **Flying in, check your DMs!**"
+                    )
+                    await ctx.message.delete()
+                    await ctx.send(embed=msg_confirmation)
+
+        else:
+            # User does not have permissions.
+            no_perms_embed = Embed(
+                colour=0xf44336,
+                description="**Sorry, but you do not have the required permissions to use this command.**"
+            )
+            await ctx.send(embed=no_perms_embed)
+
+    # Secrets of the Universe that must not be revealed at any cost.
+    # m: SHVDJWx1S2lianFnSE8qfUp1M0BLMzhDS3VvaUhdcnI= k: beano
+
     @command(aliases=["a", "al"])
     async def assess(self, ctx: Context, challenge_num: int):
         """
@@ -164,7 +310,7 @@ class Cyber(Cog):
         NO_HINTS_MSG = f"**:warning: Remember, other people can't give hints after challenge {HINTS_LIMIT}**"
 
         # Gather Assess data from JSON file.
-        with open("bot/data/assess.json") as f:
+        with open("cdbot/data/assess.json") as f:
             assess_docs = load(f)
 
         if not 0 < challenge_num <= len(assess_docs):
