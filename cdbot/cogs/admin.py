5eeee286c115396f379172902972c7dfe3ec4809
Aaron <teamshortcut@users.noreply.github.com>
2019-04-14 20:56:43 +0100

    Create admin.py

---
+++ cdbot/cogs/admin.py
@@ -0,0 +1,55 @@
+import re
+from discord import Member
+from discord.ext.commands import Bot, Cog, Context, command
+
+from cbot.constants import (
+    PLACEHOLDER_NICKNAME, NICKNAME_PATTERNS
+)
+
+def checkName(nick):
+    result = False
+    for i in NICKNAME_PATTERNS:
+        if re.match(i, nick):
+            result = True
+            break
+
+    return result
+
+
+class Admin(Cog):
+    """
+    Admin functionality
+    """
+
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @Cog.listener()  # triggered on new/removed nickname
+    async def on_member_update(memberBefore, memberAfter):
+        newNickname = memberAfter.display_name
+        if checkName(newNickname):  # bad display name
+            if not checkName(memberAfter.name):  # username is okay
+                await memberAfter.edit(nick=None)  # reset nickname
+            else:
+                # assign placeholder nickname
+                await memberAfter.edit(nick=PLACEHOLDER_NICKNAME)
+    
+
+    @Cog.listener()  # triggered on username change
+    async def on_user_update(memberBefore, memberAfter):
+        newUsername = memberAfter.name
+        if checkName(newUsername):  # bad username
+            # assign placeholder nickname
+            await memberAfter.edit(nick=PLACEHOLDER_NICKNAME)
+
+
+    @Cog.listener()
+    async def on_member_join(member):
+        username = member.name
+        if checkName(username):  # bad username
+            # assign placeholder nickname
+            await member.edit(nick=PLACEHOLDER_NICKNAME)
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
