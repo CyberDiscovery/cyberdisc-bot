89bad38ae02fdf4c9fc1319115317bc358081a1b
Daniel Milnes <thebeanogamer@gmail.com>
2019-04-20 00:44:59 +0100

    Add Stackdriver debugger support

--- cdbot/__init__.py
+++ cdbot/__init__.py
@@ -1,6 +1,8 @@
 """Initialise cdbot as a package for poetry."""
 
 import sentry_sdk
+from git import Repo
+import os
 
 from .bot import bot
 from .constants import BOT_TOKEN, SENTRY_URL
@@ -8,5 +10,15 @@ from .constants import BOT_TOKEN, SENTRY_URL
 
 def main():
     """Entry point for poetry script."""
+    try:
+        import googleclouddebugger
+        path = os.path.dirname(os.path.abspath(__file__))
+        path = '/'.join(path.split('/')[:-2])
+        googleclouddebugger.enable(
+            module='cdbot',
+            version=str(Repo(path).commit())
+        )
+    except ImportError:
+        pass
     sentry_sdk.init(SENTRY_URL)
     bot.run(BOT_TOKEN)
