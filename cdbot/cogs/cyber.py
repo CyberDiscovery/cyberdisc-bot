1c897489f1d4d4364bc1dc8f05147161f114bb7a
Daniel Milnes <thebeanogamer@gmail.com>
2019-06-03 18:25:04 +0100

    Fix linting issues

--- cdbot/cogs/cyber.py
+++ cdbot/cogs/cyber.py
@@ -8,7 +8,9 @@ from json import load
 
 from aiohttp import ClientSession
 from cdbot.constants import (
-    BASE_ALIASES, CYBERDISC_ICON_URL, ELITE_ROLE_ID, ELITE_LDN_OLD_ROLE_ID, ELITE_BRM_OLD_ROLE_ID, ELITE_LAN_OLD_ROLE_ID, ELITE_LDN_YNG_ROLE_ID, ELITE_BRM_YNG_ROLE_ID, ELITE_LAN_YNG_ROLE_ID, ELITE_EXCH_LIST_ROLE_ID, ELITE_EXCH_CONF_ROLE_ID, END_README_MESSAGE, HINTS_LIMIT, HUNDRED_PERCENT_ROLE_ID, ROOT_ROLE_ID,
+    BASE_ALIASES, CYBERDISC_ICON_URL, ELITE_BRM_OLD_ROLE_ID, ELITE_BRM_YNG_ROLE_ID, ELITE_EXCH_CONF_ROLE_ID,
+    ELITE_EXCH_LIST_ROLE_ID, ELITE_LAN_OLD_ROLE_ID, ELITE_LAN_YNG_ROLE_ID, ELITE_LDN_OLD_ROLE_ID,
+    ELITE_LDN_YNG_ROLE_ID, ELITE_ROLE_ID, END_README_MESSAGE, HINTS_LIMIT, HUNDRED_PERCENT_ROLE_ID, ROOT_ROLE_ID,
     TRUE_HUNDRED_PERCENT_ROLE_ID
 )
 from dateutil.parser import parse
@@ -375,15 +377,15 @@ class Cyber(Cog):
         exch_l_r = ctx.guild.get_role(ELITE_EXCH_LIST_ROLE_ID)
         exch_c_r = ctx.guild.get_role(ELITE_EXCH_CONF_ROLE_ID)
 
-
-        await ctx.send(f"""There are {len(game_r.members)} that have qualified for CyberStart Elite, with {len(exch_l_r.members)} qualifying for Exchange ({len(exch_c_r.members)} confirmed).
+        await ctx.send(f"""There are {len(elite_r.members)} that have qualified for CyberStart Elite.
+        {len(exch_l_r.members)} members have qualified for Exchange ({len(exch_c_r.members)} confirmed).
         Of those who didn't, preferences have been expressed as follows:
         London - Younger: {len(ldn_y_r.members)}
         London - Older: {len(ldn_o_r.members)}
         Birmingham - Younger: {len(brm_y_r.members)}
         Birmingham - Older: {len(brm_o_r.members)}
         Lancaster - Younger: {len(lan_y_r.members)}
-        Lancaster - Older:{len(lan_y_r.members)}
+        Lancaster - Older:{len(lan_o_r.members)}
         """)
 
     async def countdown(self, countdown_target_str: str, stage_name: str, ctx: Context):
