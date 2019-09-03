bbf465d450c8ac9d14c1f56f34dc0853a575fd3f
Daniel Milnes <thebeanogamer@gmail.com>
2019-09-03 11:18:50 +0100

    Fix Countdown Dates

--- cdbot/cogs/cyber.py
+++ cdbot/cogs/cyber.py
@@ -338,7 +338,7 @@ class Cyber(Cog):
         Gets the date of, and days and months until, CyberStart Game
         """
 
-        await self.countdown('15th January 2019', 'CyberStart Game', ctx)
+        await self.countdown('5th November 2019', 'CyberStart Game', ctx)
 
     @command()
     async def essentials(self, ctx: Context):
@@ -346,7 +346,7 @@ class Cyber(Cog):
         Gets the date of, and days and months until, CyberStart Essentials
         """
 
-        await self.countdown('5th March 2019', 'CyberStart Essentials', ctx)
+        await self.countdown('16th December 2019', 'CyberStart Essentials', ctx)
 
     @command()
     async def hundred(self, ctx: Context):
