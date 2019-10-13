40a8cf525a7b3fcd956ab05963a393f0d7b0183f
Jeremiah Boby <mail@jeremiahboby.me>
2019-10-13 16:04:34 +0100

    Remove `migrate_quotes` command, moving logic to __init__

--- cdbot/cogs/fun.py
+++ cdbot/cogs/fun.py
@@ -103,6 +103,23 @@ class Fun(Cog):
         self.quote_channel = None
         self.fake_staff_role = None
 
+        # Create and initialise the `quotes` table with user quotes.
+        conn = await asyncpg.connect(
+            host=PostgreSQL.PGHOST,
+            port=PostgreSQL.PGPORT,
+            user=PostgreSQL.PGUSER,
+            password=PostgreSQL.PGPASSWORD,
+            database=PostgreSQL.PGDATABASE,
+        )
+        await conn.execute(
+            "CREATE TABLE IF NOT EXISTS quotes (quote_id bigint PRIMARY KEY, author_id bigint)"
+        )
+        quote_channel = self.bot.get_channel(QUOTES_CHANNEL_ID)
+        async for quote in quote_channel.history(limit=None):
+            await self.add_quote_to_db(conn, quote)
+        await conn.close()
+        await ctx.send("Done!")
+
     @Cog.listener()
     async def on_ready(self):
         guild = self.bot.guilds[0]
@@ -354,29 +371,6 @@ class Fun(Cog):
         else:
             await conn.execute("INSERT INTO quotes(quote_id) VALUES($1) ON CONFLICT DO NOTHING;", quote.id)
         print(f"Quote ID: {quote.id} has been added to the database.")
-
-    @command()
-    @has_any_role(*ADMIN_ROLES)
-    async def migrate_quotes(self, ctx: Context):
-        """
-        Pulls all quotes from a quotes channel into a PostgreSQL database.
-        Needs PGHOST, PGPORT, PGUSER, PGDATABASE and PGPASSWORD env vars.
-        """
-        conn = await asyncpg.connect(
-            host=PostgreSQL.PGHOST,
-            port=PostgreSQL.PGPORT,
-            user=PostgreSQL.PGUSER,
-            password=PostgreSQL.PGPASSWORD,
-            database=PostgreSQL.PGDATABASE,
-        )
-        await conn.execute(
-            "CREATE TABLE IF NOT EXISTS quotes (quote_id bigint PRIMARY KEY, author_id bigint)"
-        )
-        quote_channel = self.bot.get_channel(QUOTES_CHANNEL_ID)
-        async for quote in quote_channel.history(limit=None):
-            await self.add_quote_to_db(conn, quote)
-        await conn.close()
-        await ctx.send("Done!")
 
     async def create_text_image(self, ctx: Context, person: str, text: str):
         """
