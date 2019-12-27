9db90803c8d3f5c7146008a0742f3d48e5eea5cd
Yudhajit N <28938427+Sh3llcod3@users.noreply.github.com>
2019-12-27 13:07:06 +0000

    Update cyber.py

--- cdbot/cogs/cyber.py
+++ cdbot/cogs/cyber.py
@@ -54,9 +54,11 @@ class Cyber(Cog):
          ),
 
         # Other events
-        (r"^.*\bwhat\b.*\b((gci)|(google\b.*\bcode))\b.*$", 
-         ("Google Code-in is a global, open source competition where students aged 13-17 contribute to open source projects and earn prizes."
-          " Find out more at https://codein.withgoogle.com/ or join the Discord server at https://discord.gg/rGwYUF9")
+        (r"^.*\bwhat\b.*\b((gci)|(google\b.*\bcode))\b.*$",
+         ("Google Code-in is a global, open source competition where students aged 13-17 contribute"
+          " to open source projects and earn prizes."
+          " Find out more at https://codein.withgoogle.com/ or join the Discord server at https://discord.gg/rGwYUF9"
+         )
         )
     ]
 
