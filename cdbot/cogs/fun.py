2a7dc37ce5a7ae99c2d9cbf72c4d733b20364937
Daniel Milnes <thebeanogamer@gmail.com>
2020-01-09 14:51:20 +0000

    Percentages are hard

--- cdbot/cogs/fun.py
+++ cdbot/cogs/fun.py
@@ -358,8 +358,8 @@ class Fun(Cog):
             await ctx.send(f"There are {total_quotes} quotes in the database")
         else:
             user_quotes = await conn.fetchval('SELECT count(*) FROM quotes WHERE author_id=$1;', member.id)
-            await ctx.send(f"There are {total_quotes} quotes from {member} in the database \
-({((user_quotes - total_quotes) / total_quotes) * 100}%)")
+            await ctx.send(f"There are {user_quotes} quotes from {member} in the database \
+({(user_quotes / total_quotes) * 100}%)")
 
     async def add_quote_to_db(self, conn: asyncpg.connection.Connection, quote: Message):
         """
