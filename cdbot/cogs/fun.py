60613bcfdf61cd62d1c27220763e05bb678009c9
Daniel Milnes <thebeanogamer@gmail.com>
2020-01-10 13:14:28 +0000

    Remove unused import

--- cdbot/cogs/fun.py
+++ cdbot/cogs/fun.py
@@ -28,7 +28,7 @@ from cdbot.constants import (
 from discord import Colour, Embed, File, HTTPException, Message, NotFound, embeds
 from discord.ext.commands import (
     Bot, BucketType, Cog,
-    Context, UserConverter, command, cooldown, UserConverter
+    Context, UserConverter, command, cooldown
 )
 from discord.utils import get
 
