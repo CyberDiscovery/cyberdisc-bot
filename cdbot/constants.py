e4170e58e1c7de712bb526d558df3a120bee6bb1
Daniel Milnes <thebeanogamer@gmail.com>
2020-01-23 16:15:51 +0000

    Enable EliteCount

--- cdbot/constants.py
+++ cdbot/constants.py
@@ -71,7 +71,7 @@ class Roles:
 # Cyber Constants
 HINTS_LIMIT = 8
 CYBERDISC_ICON_URL = "https://pbs.twimg.com/profile_images/921313066515615745/fLEl2Gfa_400x400.jpg"
-ELITECOUNT_ENABLED = False
+ELITECOUNT_ENABLED = True
 
 # Readme command constants
 README_SEND_ALIASES = ["create", "push", "generate", "send", "make", "build", "upload"]
