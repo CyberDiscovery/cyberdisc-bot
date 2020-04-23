fef4335a1b5eb3850c1b571eb18e072cc119f929
Daniel Milnes <thebeanogamer@gmail.com>
2020-04-23 22:11:50 +0100

    Sentry release tracking (#406)
    * Set Sentry release
    
    * Add Sentry to release pipeline
    
    * Combine steps
    
    * Enable Sentry AIOHTTP

--- cdbot/__init__.py
+++ cdbot/__init__.py
@@ -1,6 +1,8 @@
 """Initialise cdbot as a package for poetry."""
 
 import sentry_sdk
+from git import Repo
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 
 from .bot import bot
 from .constants import BOT_TOKEN, SENTRY_URL
@@ -8,5 +10,9 @@ from .constants import BOT_TOKEN, SENTRY_URL
 
 def main():
     """Entry point for poetry script."""
-    sentry_sdk.init(SENTRY_URL)
+    sentry_sdk.init(
+        SENTRY_URL,
+        release=Repo().head.object.hexsha,
+        integrations=[AioHttpIntegration()],
+    )
     bot.run(BOT_TOKEN)
