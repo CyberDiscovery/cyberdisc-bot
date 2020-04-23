39e3c5d66dd37c2ee1079df2e9c67e7dd385278f
Daniel Milnes <thebeanogamer@gmail.com>
2020-04-23 21:19:51 +0100

    Set Sentry release

--- cdbot/__init__.py
+++ cdbot/__init__.py
@@ -1,6 +1,7 @@
 """Initialise cdbot as a package for poetry."""
 
 import sentry_sdk
+from git import Repo
 
 from .bot import bot
 from .constants import BOT_TOKEN, SENTRY_URL
@@ -8,5 +9,5 @@ from .constants import BOT_TOKEN, SENTRY_URL
 
 def main():
     """Entry point for poetry script."""
-    sentry_sdk.init(SENTRY_URL)
+    sentry_sdk.init(SENTRY_URL, release=Repo().head.object.hexsha)
     bot.run(BOT_TOKEN)
