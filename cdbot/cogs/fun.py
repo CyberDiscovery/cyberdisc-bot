fd356d530ccb6e187c528fb0282fa8aaaf03c892
Daniel Milnes <thebeanogamer@gmail.com>
2020-07-01 20:39:08 +0100

    Don't try and connect to database if environ set

--- cdbot/cogs/fun.py
+++ cdbot/cogs/fun.py
@@ -38,6 +38,7 @@ from cdbot.constants import (
     CYBERDISC_ICON_URL,
     EMOJI_LETTERS,
     FAKE_ROLE_ID,
+    LOCAL_DEBUGGING,
     PostgreSQL,
     QUOTES_BOT_ID,
     QUOTES_CHANNEL_ID,
@@ -140,15 +141,16 @@ class Fun(Cog):
         if self.fake_staff_role is None:
             self.fake_staff_role = guild.get_role(FAKE_ROLE_ID)
 
-        self.bot.pool = await asyncpg.create_pool(
-            host=PostgreSQL.PGHOST,
-            port=PostgreSQL.PGPORT,
-            user=PostgreSQL.PGUSER,
-            password=PostgreSQL.PGPASSWORD,
-            database=PostgreSQL.PGDATABASE,
-        )
+        if not LOCAL_DEBUGGING:
+            self.bot.pool = await asyncpg.create_pool(
+                host=PostgreSQL.PGHOST,
+                port=PostgreSQL.PGPORT,
+                user=PostgreSQL.PGUSER,
+                password=PostgreSQL.PGPASSWORD,
+                database=PostgreSQL.PGDATABASE,
+            )
+            await self.migrate_quotes()
 
-        await self.migrate_quotes()
 
     @cooldown(1, 60, BucketType.user)
     @cooldown(4, 60, BucketType.channel)
--- cdbot/constants.py
+++ cdbot/constants.py
@@ -125,6 +125,8 @@ CYBERDISC_ICON_URL = (
 )
 ELITECOUNT_ENABLED = True
 
+LOCAL_DEBUGGING = bool(environ.get("LOCAL_DEBUGGING", False))
+
 # Readme command constants
 README_SEND_ALIASES = ["create", "push", "generate", "send", "make", "build", "upload"]
 README_RECV_ALIASES = ["fetch", "get", "pull", "download", "retrieve", "dm", "dl"]
