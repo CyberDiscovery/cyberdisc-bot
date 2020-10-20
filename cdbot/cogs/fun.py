fcc6756b8567099655b85a0c8378d588a8ce4503
HackerJacker <56278210+H4ckerJ4cker@users.noreply.github.com>
2020-10-20 16:16:01 +0100

    Added quotes system

--- cdbot/cogs/fun.py
+++ cdbot/cogs/fun.py
@@ -103,7 +103,7 @@ class Fun(Cog):
         )
         .set_footer(
             text="To continue with the ping, react \N{THUMBS UP SIGN}, To delete this message and move on,"
-            " react \N{THUMBS DOWN SIGN}"
+                 " react \N{THUMBS DOWN SIGN}"
         )
         .add_field(
             name="Cyber Discovery staff will not provide help for challenges.",
@@ -159,7 +159,7 @@ class Fun(Cog):
     async def on_message(self, message: Message):
         # If a new quote is added, add it to the database.
         if message.channel.id == QUOTES_CHANNEL_ID and (
-            message.author.id == QUOTES_BOT_ID or message.mentions is not None
+                message.author.id == QUOTES_BOT_ID or message.mentions is not None
         ):
             await self.add_quote_to_db(message)
             print(f"Message #{message.id} added to database.")
@@ -232,10 +232,10 @@ class Fun(Cog):
 
         # Adds waving emoji when a new user joins.
         if all(
-            (
-                "Welcome to the Cyber Discovery" in message.content,
-                message.author.id == WELCOME_BOT_ID,
-            )
+                (
+                    "Welcome to the Cyber Discovery" in message.content,
+                    message.author.id == WELCOME_BOT_ID,
+                )
         ):
             await message.add_reaction("\N{WAVING HAND SIGN}")
 
@@ -243,10 +243,10 @@ class Fun(Cog):
     async def on_raw_reaction_add(self, raw_reaction: RawReactionActionEvent):
         thumbs_down = "\N{THUMBS DOWN SIGN}"
         if all(
-            (
-                str(raw_reaction.emoji) == thumbs_down,
-                raw_reaction.channel_id == QUOTES_CHANNEL_ID,
-            )
+                (
+                    str(raw_reaction.emoji) == thumbs_down,
+                    raw_reaction.channel_id == QUOTES_CHANNEL_ID,
+                )
         ):
             quotes_channel = self.bot.get_channel(QUOTES_CHANNEL_ID)
             logs_channel = self.bot.get_channel(LOGGING_CHANNEL_ID)
@@ -367,6 +367,34 @@ class Fun(Cog):
         await ctx.send(embed=comic)
 
     @command()
+    async def quote(self, ctx: Context, message_id: int, channel=None):
+        """
+        Quotes the specified message.
+        """
+        try:
+            channel = ctx if channel is None else self.bot.get_channel(
+                int(channel[2:-1] if channel[0] == "<" else channel))
+            message = await channel.fetch_message(int(message_id))
+
+            embed = Embed(
+                description=f"{message.content}\n\n[Jump to message]({message.jump_url})",
+                colour=Colour.blue(),
+                timestamp=message.created_at,
+            )
+            embed.set_image(url=message.attachments[0].url) if message.attachments else ...
+            embed.set_author(name=f"{message.author.name}#{message.author.discriminator}",
+                             icon_url=message.author.avatar_url)
+            embed.set_footer(text=f"{message.guild.name} | #{message.channel}")
+
+            await ctx.send(embed=message.embeds[0] if message.embeds else embed)
+        except NotFound:
+            await ctx.send(":no_entry_sign: Uh oh, quote not found :(",
+                           delete_after=5 if ctx.channel.id == QUOTES_CHANNEL_ID else ...)
+        finally:
+            if ctx.channel.id == QUOTES_CHANNEL_ID:
+                await ctx.message.delete()
+
+    @command()
     async def quotes(self, ctx: Context, member: FormerUser = None):
         """
         Returns a random quotation from the #quotes channel.
@@ -444,9 +472,9 @@ class Fun(Cog):
                 return await ctx.send(":no_entry_sign: Invalid page number")
 
             for result in await connection.fetch(
-                "SELECT author_id, COUNT(author_id) as quote_count FROM quotes "
-                "GROUP BY author_id ORDER BY quote_count DESC LIMIT 10 OFFSET $1",
-                start_from,
+                    "SELECT author_id, COUNT(author_id) as quote_count FROM quotes "
+                    "GROUP BY author_id ORDER BY quote_count DESC LIMIT 10 OFFSET $1",
+                    start_from,
             ):
                 author, quotes = result.values()
                 users += f"{start_from + current}. <@{author}> - {quotes}\n"
