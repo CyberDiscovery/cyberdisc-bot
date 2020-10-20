54afa8dd1e6abd5bc046c4b6a3dbb09ee8d5cfae
HackerJacker <56278210+H4ckerJ4cker@users.noreply.github.com>
2020-10-20 16:24:12 +0100

    fixed indentation errors

--- cdbot/cogs/fun.py
+++ cdbot/cogs/fun.py
@@ -103,7 +103,7 @@ class Fun(Cog):
         )
         .set_footer(
             text="To continue with the ping, react \N{THUMBS UP SIGN}, To delete this message and move on,"
-                 " react \N{THUMBS DOWN SIGN}"
+            " react \N{THUMBS DOWN SIGN}"
         )
         .add_field(
             name="Cyber Discovery staff will not provide help for challenges.",
@@ -159,7 +159,7 @@ class Fun(Cog):
     async def on_message(self, message: Message):
         # If a new quote is added, add it to the database.
         if message.channel.id == QUOTES_CHANNEL_ID and (
-                message.author.id == QUOTES_BOT_ID or message.mentions is not None
+            message.author.id == QUOTES_BOT_ID or message.mentions is not None
         ):
             await self.add_quote_to_db(message)
             print(f"Message #{message.id} added to database.")
@@ -232,10 +232,10 @@ class Fun(Cog):
 
         # Adds waving emoji when a new user joins.
         if all(
-                (
-                    "Welcome to the Cyber Discovery" in message.content,
-                    message.author.id == WELCOME_BOT_ID,
-                )
+            (
+                "Welcome to the Cyber Discovery" in message.content,
+                message.author.id == WELCOME_BOT_ID,
+            )
         ):
             await message.add_reaction("\N{WAVING HAND SIGN}")
 
@@ -243,10 +243,10 @@ class Fun(Cog):
     async def on_raw_reaction_add(self, raw_reaction: RawReactionActionEvent):
         thumbs_down = "\N{THUMBS DOWN SIGN}"
         if all(
-                (
-                    str(raw_reaction.emoji) == thumbs_down,
-                    raw_reaction.channel_id == QUOTES_CHANNEL_ID,
-                )
+            (
+                str(raw_reaction.emoji) == thumbs_down,
+                raw_reaction.channel_id == QUOTES_CHANNEL_ID,
+            )
         ):
             quotes_channel = self.bot.get_channel(QUOTES_CHANNEL_ID)
             logs_channel = self.bot.get_channel(LOGGING_CHANNEL_ID)
@@ -472,9 +472,9 @@ class Fun(Cog):
                 return await ctx.send(":no_entry_sign: Invalid page number")
 
             for result in await connection.fetch(
-                    "SELECT author_id, COUNT(author_id) as quote_count FROM quotes "
-                    "GROUP BY author_id ORDER BY quote_count DESC LIMIT 10 OFFSET $1",
-                    start_from,
+                "SELECT author_id, COUNT(author_id) as quote_count FROM quotes "
+                "GROUP BY author_id ORDER BY quote_count DESC LIMIT 10 OFFSET $1",
+                start_from,
             ):
                 author, quotes = result.values()
                 users += f"{start_from + current}. <@{author}> - {quotes}\n"
