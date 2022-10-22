29f9ba5606c9053e18cfea77e45da93523b10871
thebeanogamer <thebeanogamer@gmail.com>
2022-10-22 23:11:46 +0100

    Remove the broken maths challenge code
    I am sorry to the people of <kingsmathsschool.com> for bullying your API all these years...

--- cdbot/cogs/maths.py
+++ cdbot/cogs/maths.py
@@ -16,102 +16,11 @@ from html2markdown import convert
 from cdbot.constants import Maths as constants
 
 
-async def get_challenges(
-    client: httpx.AsyncClient, page_index: int = 0, page_size: int = 999
-):
-    """Get challenges, given the relevant parameters."""
-    return (
-        await client.post(
-            constants.Challenges.URL,
-            headers=dict(accessToken=constants.Challenges.TOKEN),
-            json={
-                "pageIndex": page_index,
-                "pageSize": page_size,
-                "orderBy": [{"desc": "answerDate"}],
-                "where": [
-                    {"field": "sys.versionStatus", "equalTo": "published"},
-                    {"field": "sys.contentTypeId", "in": ["mathsQuiz"]},
-                ],
-                "fields": ["entryTitle", "category", "sys", "description", "answer"],
-            },
-        )
-    ).json()["items"]
-
-
-async def get_challenge(number: int) -> dict:
-    async with httpx.AsyncClient() as client:
-        challenge, *_ = await get_challenges(client, page_index=number - 1, page_size=1)
-
-        question = (
-            await client.post(
-                constants.Challenges.URL,
-                headers=dict(accessToken=constants.Challenges.TOKEN),
-                json={
-                    "pageIndex": 0,
-                    "pageSize": 1,
-                    "where": [
-                        {"field": "sys.slug", "equalTo": challenge["sys"]["slug"]},
-                        {"field": "sys.versionStatus", "equalTo": "published"},
-                    ],
-                },
-            )
-        ).json()["items"][0]["question"]
-
-    asset = question[1]["value"]["asset"]["sys"] if len(question) > 1 else None
-
-    return {
-        "title": challenge["entryTitle"],
-        "published": dateutil.parser.isoparse(
-            challenge["sys"]["version"]["created"]
-        ).strftime("%d/%m/%Y"),
-        "category": challenge["category"][0]["entryTitle"],
-        "challenge": convert(question[0]["value"]).replace("&nbsp;", "")[:-1],
-        "image": (
-            (
-                "https://www.kingsmathsschool.com"
-                "".join(
-                    asset["uri"].rpartition("/")[:2] + (asset["properties"]["filename"],)
-                )
-            )
-            if asset
-            else ""
-        ),
-        "description": challenge["description"],
-        "slug": challenge["sys"]["slug"],
-    }
-
-
 class Maths(Cog):
     """Maths-related commands."""
 
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.update_challenge.start()
-
-    @tasks.loop(minutes=1)
-    async def update_challenge(self):
-        """Check the Kings site for the latest challenges."""
-        print("Updating maths challenges...")
-        latest_challenge = float("inf")
-        latest_challenge = int(
-            self.channel.topic.split("Nerds, the lot of you | Challenge ")[1].split(
-                " "
-            )[0][:-1]
-        )
-        async with httpx.AsyncClient() as client:
-            challenges = await get_challenges(client)
-        for number, challenge in enumerate(challenges[::-1], 1):
-            title = challenge["entryTitle"]
-            if number > latest_challenge:
-                await self.challenge(self.channel, len(challenges) - number + 1)
-                await self.channel.edit(topic=constants.Challenges.TOPIC.format(title))
-        print("Maths challenges successfully updated.")
-
-    @update_challenge.before_loop
-    async def wait_until_ready(self):
-        """Wait for bot to become ready."""
-        await self.bot.wait_until_ready()
-        self.channel = self.bot.get_channel(constants.Challenges.CHANNEL)
 
     @Cog.listener()
     async def on_message(self, message):
@@ -234,8 +143,8 @@ class Maths(Cog):
                 return await ctx.send(embed=embed, delete_after=30)
 
 
-def setup(bot):
+async def setup(bot):
     """
     Required boilerplate for adding functionality of cog to bot.
     """
-    bot.add_cog(Maths(bot))
+    await bot.add_cog(Maths(bot))
